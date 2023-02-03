66740fdf92738212162229148007e691bac2679f
Jeferson Lopes <jefersonlopesluis@protonmail.com>
2023-02-03 14:02:05 +0000

    first commit

---
+++ App.java
@@ -0,0 +1,22 @@
+/**
+ * App
+ */
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] mat = sc.nextInt[n][n];
+
+        System.out.println("Digite: ");
+
+
+
+
+
+        sc.close();
+    }
+}
