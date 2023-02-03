741b006f53f5705a59f49153fa2e61a69994b811
Jeferson Lopes <jefersonlopesluis@protonmail.com>
2023-02-03 15:43:54 +0000

    subindo codigo da matriz

--- App.java
+++ App.java
@@ -9,10 +9,31 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[][] mat = sc.nextInt[n][n];
+        int[][] mat = new int[n][n];
 
-        System.out.println("Digite: ");
+        for (int i =0;i<n;i++){
+            for (int j=0;j<n;j++){
+            mat[i][j] = sc.nextInt();
+            }
+        }
 
+        System.out.println("Main diagonal: ");
+        for (int i=0;i<n;i++){
+            System.out.println(mat[i][i] + " ");
+        }
+        System.out.println();
+
+        int neg =0;
+        for (int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]<0){
+                neg++;
+                }
+                
+            }
+        }
+
+            System.out.println("Negative numbers: " + neg);
 
 
 
